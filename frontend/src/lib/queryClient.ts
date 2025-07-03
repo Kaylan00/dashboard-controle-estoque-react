c571fd866587ae7672c9b1754777f88157392442
Kaylan00 <kaylanargollo16@gmail.com>
2025-07-02 21:10:58 -0300

    fix: removing cacheTime

--- frontend/src/lib/queryClient.ts
+++ frontend/src/lib/queryClient.ts
@@ -3,10 +3,8 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-      staleTime: 5 * 60 * 1000, // 5 minutos
-      cacheTime: 10 * 60 * 1000, // 10 minutos
+    gcTime: 1000 * 60 * 60 * 24, // 24 horas
+
     },
   },
 }); 
